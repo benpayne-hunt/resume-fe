184ddd604436e163a65460d15168348cd25c03d2
Benjamin Payne-Hunt <ben@benpaynehunt.co.uk>
2024-07-03 08:45:29 +0100

    Adds basic about structure and additional styling

--- src/components/Body/About/index.tsx
+++ src/components/Body/About/index.tsx
@@ -6,10 +6,28 @@ import './About.css';
 const About = (): ReactElement => {
   return (
     <div className="About">
-      <ImageBadge
-        src="https://media.licdn.com/dms/image/D4E03AQFnJnEqI7kTWA/profile-displayphoto-shrink_400_400/0/1689328946343?e=1725494400&v=beta&t=Z9F0yelfmMZ_nsJWXi6n9-QkijIB73tCS1dgNzvL-gs"
-        alt="Ben Payne Hunt"
-      />
+      <div className="About-container">
+        <div className="About-slogan-container">
+          <h1 className="About-slogan-1">Senior. </h1>
+          <h1 className="About-slogan-2">Software. </h1>
+          <h1 className="About-slogan-3">Engineer.</h1>
+        </div>
+        <div className="About-greeting">
+          <ImageBadge
+            src="https://media.licdn.com/dms/image/D4E03AQFnJnEqI7kTWA/profile-displayphoto-shrink_400_400/0/1689328946343?e=1725494400&v=beta&t=Z9F0yelfmMZ_nsJWXi6n9-QkijIB73tCS1dgNzvL-gs"
+            alt="Ben Payne Hunt"
+          />
+          <div>
+            <h1 className="About-title">Hi there 👋</h1>
+            <h6 className="About-text">
+              I'm a Tech Lead with a passion for running effective, collaborative and fun teams that look to deliver value in every pull request we open 💪
+            </h6>
+            <h6 className="About-text">
+              Currently helping high-growth, fast-paced startup Shift disrupt the logistics industry by building a tech powered digital marketplace 🚀
+            </h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
