b96357f2d8f0dabdb71ddecdc588270518a78b5a
Benjamin Payne-Hunt <ben@benpaynehunt.co.uk>
2024-07-03 21:10:38 +0100

    Adds codeblock functionality

--- src/widgets/Buttons/CodeSightButton/components/CodeSightPopup/index.tsx
+++ src/widgets/Buttons/CodeSightButton/components/CodeSightPopup/index.tsx
@@ -1,20 +1,30 @@
 import React, { ReactElement } from 'react';
 
+import { CodeBlock, dracula } from 'react-code-blocks';
 import './CodeSightPopup.css';
 
 type Props = {
   title?: string;
   text?: string;
-  codeBlock?: Object;
+  code?: string;
+  language?: string;
 }
 
-const CodeSightPopup = ({ title, text }: Props): ReactElement => {
+const CodeSightPopup = ({ title, text, code, language }: Props): ReactElement => {
   return (
     <div className="CodeSightPopup">
       <h1 className="CodeSightPopup-title">{`${title ?? 'Code Sight'}`}</h1>
       <h6 className="CodeSightPopup-text">
         {`${text ?? 'Use this button to quickly scan through the code of any element on the page 🔍'}`}
       </h6>
+      {code ?
+        <CodeBlock
+          text={code}
+          language={language}
+          showLineNumbers={true}
+          theme={dracula}
+        />
+      : null}
     </div>
   );
 };
